e9185a8d7c7fee7610dbb6789a36cc07c983042e
felhe <10731299+felhe@users.noreply.github.com>
2023-05-04 20:58:40 +0200

    feat: add .zshrc and zsh plugins

--- dot_scripts/executable_install.sh
+++ dot_scripts/executable_install.sh
@@ -17,11 +17,13 @@ if [ -n "$(command -v apt-get)" ]; then
     echo -e "${BLUE}Detected Ubuntu based system${NC}"
     sudo apt-get update
     sudo apt-get install -y "${PACKAGES[@]}"
+    chsh -s $(which zsh)
 elif [ -n "$(command -v dnf)" ]; then
     # Fedora based system
     echo -e "${BLUE}Detected Fedora based system${NC}"
     sudo dnf update
     sudo dnf install -y "${PACKAGES[@]}"
+    sudo lchsh $USER
 else
     echo -e "${BLUE}Unsupported distribution${NC}"
     exit 1
@@ -29,11 +31,15 @@ fi
 
 # Install oh-my-zsh
 echo -e "${BLUE}Installing oh-my-zsh${NC}"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
 
 # Install zsh-autosuggestions
 echo -e "${BLUE}Installing zsh-autosuggestions${NC}"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting
+echo -e "${BLUE}Installing zsh-syntax-highlighting${NC}"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Install starship
 echo -e "${BLUE}Installing starship${NC}"
